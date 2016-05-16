10847157cbabcee10204c39a997e055f647c0ca0
Boris <zhouboli@gmail.com>
2016-05-16 15:57:43 +0800

    added one more medium

--- src/Leet.java
+++ src/Leet.java
@@ -39,6 +39,18 @@ class ListNode
 }
 
 public class Leet {
+
+    /**
+     * Lowest Common Ancestor of a Binary Tree
+     */
+    public TreeNode lowestCommonAncestorInBinaryTree(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left != null && right != null ? root : left != null ? left : right;
+    }
+
     /**
      * Binary Tree Paths
      */
